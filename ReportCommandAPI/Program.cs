42a272da801e807484939ef6e210e5eb5fe179c3
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-11-28 21:41:01 +0100

    added reportcommand endpoints + docker-compose elements

--- ReportCommandAPI/Program.cs
+++ ReportCommandAPI/Program.cs
@@ -12,6 +12,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GPReportCommandService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => "health check");
 
 app.Run();
--- ReportCommandAPI/Services/GPReportCommandService.cs
+++ ReportCommandAPI/Services/GPReportCommandService.cs
@@ -1,21 +1,88 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using ReportCommandAPI;
+using ReportCommandAPI.Data;
+using ReportCommandAPI.Models;
 
 namespace ReportCommandAPI.Services
 {
     public class GPReportCommandService : GPReportCommandProto.GPReportCommandProtoBase
     {
-        private readonly ILogger<GPReportCommandService> _logger;
-        public GPReportCommandService(ILogger<GPReportCommandService> logger)
+        private readonly ReportCommandDbContext _dbContext;
+        public GPReportCommandService(ReportCommandDbContext dbContext)
         {
-            _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public override Task<CreateGPReportResponse> CreateGPReport(CreateGPReportRequest request, ServerCallContext context)
+        public override async Task<CreateGPReportResponse> CreateGPReport(CreateGPReportRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new CreateGPReportResponse
+            if (request.Notes == string.Empty)
             {
-                Id = "Hello " + request
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "You must provide a valid input"));
+            }
+
+            var gpreport = new GPReportDTO
+            {
+                PatientId = Guid.Parse(request.PatientId),
+                EmployeeId = Guid.Parse(request.EmployeeId),
+                Notes = request.Notes,
+            };
+
+            await _dbContext.AddAsync(gpreport);
+            await _dbContext.SaveChangesAsync();
+
+            return await Task.FromResult(new CreateGPReportResponse
+            {
+                Id = gpreport.Id.ToString()
+            });
+        }
+
+        public override async Task<UpdateGPReportResponse> UpdateGPReport(UpdateGPReportRequest request, ServerCallContext context)
+        {
+            if (request.Id == string.Empty || request.PatientId == string.Empty || request.EmployeeId == string.Empty || request.Notes == string.Empty)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "You must provide a valid input"));
+            }
+            var gpreport = await _dbContext.GPReports.FirstOrDefaultAsync(r => r.Id.ToString() == request.Id);
+
+            if (gpreport == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"No gpreport with id {request.Id}"));
+            }
+
+            gpreport.Id = Guid.Parse(request.Id);
+            gpreport.PatientId = Guid.Parse(request.PatientId);
+            gpreport.EmployeeId = Guid.Parse(request.EmployeeId);
+            gpreport.Notes = request.Notes;
+
+            await _dbContext.SaveChangesAsync();
+
+            return await Task.FromResult(new UpdateGPReportResponse
+            {
+                Id = gpreport.Id.ToString()
+            });
+        }
+
+        public override async Task<DeleteGPReportResponse> DeleteGPReport(DeleteGPReportRequest request, ServerCallContext context)
+        {
+            if (request.Id == string.Empty)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "You must provide a valid input"));
+            }
+            var gpreport = await _dbContext.GPReports.FirstOrDefaultAsync(r => r.Id.ToString() == request.Id);
+
+            if (gpreport == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"No gpreport with id {request.Id}"));
+            }
+
+            _dbContext.GPReports.Remove(gpreport);
+            await _dbContext.SaveChangesAsync();
+
+            return await Task.FromResult(new DeleteGPReportResponse
+            {
+                Id = gpreport.Id.ToString()
             });
         }
     }
