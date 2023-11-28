13d7ef007bfa3fa2b8d10653c54293ef6c40e8ed
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-11-28 21:41:01 +0100

    added reportcommand endpoints + docker-compose elements

---
+++ ReportCommandAPI/Data/ReportCommandDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportCommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportCommandAPI.Data
+{
+    public class ReportCommandDbContext : DbContext
+    {
+        public ReportCommandDbContext(DbContextOptions<ReportCommandDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<GPReportDTO> GPReports => Set<GPReportDTO>();
+    }
+}
