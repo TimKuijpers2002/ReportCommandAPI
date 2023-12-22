41676d06d7428c9d85805d5e72e566b1e3e7ec59
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-12-22 13:22:48 +0100

    typo

--- ReportCommandAPI/Services/GPReportCommandService.cs
+++ ReportCommandAPI/Services/GPReportCommandService.cs
@@ -33,7 +33,7 @@ namespace ReportCommandAPI.Services
 
             // Prepare the statement
             var statement = new SimpleStatement(
-                "INSERT INTO gpreport (Id, PatientId, EmployeeId, InitialCreation Notes) VALUES (?, ?, ?, ?, ?)",
+                "INSERT INTO gpreport (Id, PatientId, EmployeeId, Notes, InitialCreation) VALUES (?, ?, ?, ?, ?)",
                 gpreport.Id, gpreport.PatientId, gpreport.EmployeeId, gpreport.Notes, gpreport.InitialCreation);
 
             // Execute the statement
